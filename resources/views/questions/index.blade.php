4c2fe15235e7f5fc381539cba419a669657e9ea3
dancinturtle <amy.giver@gmail.com>
2020-09-06 16:29:06 -0700

    fetch data and display

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -2,6 +2,7 @@
 @section ('header', 'Questions asked so far')
 @section ('content')
   <h5>Click a question to view the answers given, or submit your own!</h5>
+
   <table class="table table-dark">
     <thead>
       <tr>
@@ -11,16 +12,13 @@
       </tr>
     </thead>
     <tbody>
+      @foreach ($questions as $question)
       <tr>
-        <th scope="row">Jan 3, 2020</th>
-        <td><a href="{{route('questions.show', '1')}}">Question</a></td>
-        <td>5</td>
+        <th scope="row">{{date('F j, Y', strtotime($question->created_at))}}</th>
+        <td><a href="{{route('questions.show', $question)}}">{{$question->question}}</a></td>
+        <td>{{$question->answers_count}}</td>
       </tr>
-      <tr>
-        <th scope="row">Jan 1, 2020</th>
-        <td><a href="{{route('questions.show', '2')}}">Question</a></td>
-        <td>3</td>
-      </tr>
+      @endforeach
     </tbody>
   </table>
 @endsection
