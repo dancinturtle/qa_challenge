d80e2826c077ecb43ba0eb780445bc066bdf23b7
dancinturtle <amy.giver@gmail.com>
2020-09-06 16:29:06 -0700

    fetch data and display

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class QuestionController extends Controller
 {
-    public function index() {
-        return view('questions.index');
-    }
+  public function index() {
+    $questionCollection = Question::withCount(['answers'])->get();
+    $questions = $questionCollection->sortByDesc('created_at');
+    return view('questions.index', compact('questions'));
+  }
 
-    public function create() {
-        return view('questions.createQuestion');
-    }
+  public function create() {
+    return view('questions.createQuestion');
+  }
 
-    public function show($question) {
-        return view ('questions.answers');
-    }
+  public function show(Question $question) {
+    $answers = $question->answers->sortBy('created_at');
+    $labelText = count($answers) > 0 ? "Don't like these answers? Post your own!" : "No answers have been posted yet, be the first!";
+    return view ('questions.answers', compact('question', 'answers', 'labelText'));
+  }
 }
--- resources/views/questions/answers.blade.php
+++ resources/views/questions/answers.blade.php
@@ -1,31 +1,30 @@
 @extends ('layout')
-@section ('header', 'Answers to this question')
+@section ('header', 'Answers to: ' . $question->question)
 @section ('content')
   <form>
     <div class="form-group">
-      <label for="answer">Don't like these answers? Post your own!</label>
+      <label for="answer">{{$labelText}}</label>
       <input type="text" class="form-control" id="answer" aria-describedby="questionHelp">
       <small id="answerHelp" class="form-text text-muted">5 character minimum</small>
     </div>
       <button type="submit" class="btn btn-primary">Submit</button>
   </form>
-
-  <table class="table table-dark mt-5">
-    <thead>
-      <tr>
-        <th scope="col">Date answered</th>
-        <th scope="col">Answer</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">Jan 3, 2020</th>
-        <td>Answer to question</td>
-      </tr>
-      <tr>
-        <th scope="row">Jan 5, 2020</th>
-        <td>Answer to question as well</td>
-      </tr>
-    </tbody>
-  </table>
+  @if(count($answers) > 0)
+    <table class="table table-dark mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Date answered</th>
+          <th scope="col">Answer</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($answers as $answer)
+        <tr>
+          <th scope="row">{{date('F j, Y', strtotime($answer->created_at))}}</th>
+          <td>{{$answer->answer}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @endif
 @endsection
