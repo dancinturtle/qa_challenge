f871cb4e50d3c86fde067231e3daedd6be46e70b
dancinturtle <amy.giver@gmail.com>
2020-09-06 18:14:26 -0700

    post questions and answers with validations

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+  protected $fillable = [
+    'answer',
+    'question_id'
+  ];
+
+  public function path() {
+    return route('questions.show', $this->question);
+  }
+
+  public function question() {
+    return $this->belongsTo(Question::class);
+  }
 }
+
--- resources/views/questions/createQuestion.blade.php
+++ resources/views/questions/createQuestion.blade.php
@@ -2,23 +2,35 @@
 
 @section ('content')
 <h1 class="mt-3">Ask your question</h1>
-<form>
+<form action="{{route('questions.store')}}" method="POST">
+  @csrf
   <div class="form-group">
     <label for="question">Question</label>
     <input
       type="text"
-      class="form-control"
+      class="form-control
+        {{$errors->has('question') ? 'is-invalid' : ''}}
+      "
       id="question"
+      name="question"
       aria-describedby="questionHelp"
       placeholder="Is yeast vegan?"
+      value="{{old('question')}}"
     >
     <small
       id="questionHelp"
-      class="form-text text-muted"
+      class="form-text
+        {{$errors->has('question') ? 'text-danger' : 'text-muted'}}
+      "
     >
-      5 character minimum and end with a question mark please
+      @if($errors->has('question'))
+        {{$errors->first('question')}}
+      @else
+        5 character minimum and end with a question mark please.
+      @endif
     </small>
   </div>
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 @endsection
