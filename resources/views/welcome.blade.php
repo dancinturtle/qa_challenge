0f134d3c8d7a44204f60c3672b3f362693dfd219
dancinturtle <amy.giver@gmail.com>
2020-09-06 21:24:10 -0700

    beautify with pictures, improve contrast of question links for a11y

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,31 @@
 @extends ('layout')
-@section ('header', 'Veganism: Your questions answered here')
+@section ('header', "Let's talk about veganism")
 @section ('content')
-<p>Some gibberish about veganism and welcoming people i dunno</p>
+
+  <div class="card mb-3">
+    <div class="row">
+      <div class="col">
+        <img class="img-thumbnail img-fluid" src="{{asset('images/green.jpg')}}" alt="Green sign for new vegans">
+      </div>
+      <div class="col">
+        <div class="card-body">
+          <h5 class="card-title">Ask questions</h5>
+          <p class="card-text">Veganism, you know it is the way. Is something keeping you from getting started? <a href="{{route('questions.create')}}"> Ask away</a> and see what the vegan community here has to say!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <div class="row">
+      <div class="col">
+        <img class="img-thumbnail img-fluid" src="{{asset('images/chilies.jpg')}}" alt="Dried chilies for old vegans">
+      </div>
+      <div class="col">
+        <div class="card-body">
+          <h5 class="card-title">Answer questions</h5>
+          <p class="card-text">Or perhaps you're a seasoned vegan with tips to share with the newcomers. Please <a href="{{route('questions.index')}}">view people's burning questions here</a> and grace us with your wisdom!</p>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
